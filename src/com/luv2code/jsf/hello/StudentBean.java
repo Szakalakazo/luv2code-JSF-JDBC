8e49b1337844e2e4110d551f5a98b0239a794c2c
Szakalakazo <tomek.starzak@gmail.com>
2019-02-06 16:04:35 +0100

    Add drop-down lists, radiobutton & checkboxes

---
+++ src/com/luv2code/jsf/hello/StudentBean.java
@@ -0,0 +1,74 @@
+package com.luv2code.jsf.hello;
+
+import java.util.*;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class StudentBean {
+
+	private String firstName;
+	private String lastName;
+	private String country;
+	List<String> countryOptions;
+	private String favoriteLanguage;
+	private String[] favoriteTextEditors;
+
+
+	// create no-args constructor
+
+	
+	public StudentBean() {
+		countryOptions = new ArrayList<>();
+		countryOptions.add("Brazli");
+		countryOptions.add("France");
+		countryOptions.add("Germany");
+		countryOptions.add("Poland");
+		countryOptions.add("UK");
+				
+	}
+	
+	public String[] getFavoriteTextEditors() {
+		return favoriteTextEditors;
+	}
+	
+	public void setFavoriteTextEditors(String[] favoriteTextEditors) {
+		this.favoriteTextEditors = favoriteTextEditors;
+	}
+
+	public String getFavoriteLanguage() {
+		return favoriteLanguage;
+	}
+
+	public void setFavoriteLanguage(String favoriteLanguage) {
+		this.favoriteLanguage = favoriteLanguage;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public List<String> getCountryOptions() {
+		return countryOptions;
+	}
+}

94078e805cd7c4b67209cadb0b0dfc91a429173e
Szakalakazo <tomek.starzak@gmail.com>
2019-02-07 08:59:43 +0100

    Add String and digit validation

---
+++ src/com/luv2code/jsf/validation/ValidationBean.java
@@ -0,0 +1,66 @@
+package com.luv2code.jsf.validation;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class ValidationBean {
+	
+	private String firstName;
+	private String lastName;
+	private String postalCode;
+	private String phoneNumber;
+	private int freePasses;
+	
+
+	
+	public ValidationBean() {
+		
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getFreePasses() {
+		return freePasses;
+	}
+
+	public void setFreePasses(int freePasses) {
+		this.freePasses = freePasses;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+
+
+}
+ 
